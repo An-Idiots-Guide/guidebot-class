8ff30f499a975584fa3fdad5f6d63199f7b1ec34
AndrewJWin <fastroute66@live.com>
2017-10-30 18:17:20 -0500

    Updated files

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,13 +1,13 @@
 // This event executes when a new guild (server) is joined.
 
 module.exports = class {
-  constructor(client) {
-    this.client = client;
-  }
+    constructor(client) {
+        this.client = client;
+    }
 
-  async run(guild) {
+    async run(guild) {
 
-    // We need to add this guild to our settings!
-    this.client.settings.insert({id: guild.id, settings: this.client.config.defaultSettings}).run();
-  }
+        // We need to add this guild to our settings!
+        this.client.settings.insert({ id: guild.id, settings: this.client.config.defaultSettings }).run();
+    }
 };
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,13 +1,13 @@
 // This event executes when a new guild (server) is left.
 
 module.exports = class {
-  constructor(client) {
-    this.client = client;
-  }
+    constructor(client) {
+        this.client = client;
+    }
 
-  async run(guild) {
+    async run(guild) {
 
-    // Well they're gone. Let's remove them from the settings!
-    this.client.settings.get(guild.id).delete().run();
-  }
+        // Well they're gone. Let's remove them from the settings!
+        this.client.settings.get(guild.id).delete().run();
+    }
 };
