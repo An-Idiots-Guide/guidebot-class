30665e889668beddcc8b83013162e8cfcfe9ed8b
AndrewJWin <fastroute66@live.com>
2017-10-31 11:34:39 -0500

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,23 +1,23 @@
 // This event executes when a new member joins a server. Let's welcome them!
 
 module.exports = class {
-    constructor(client) {
-        this.client = client;
-    }
+  constructor(client) {
+    this.client = client;
+  }
 
-    async run(member) {
+  async run(member) {
 
-        // Load the guild's settings
-        const settings = await this.client.settings.get(member.guild.id).getField("settings").run();
+    // Load the guild's settings
+    const settings = this.client.settings.get(member.guild.id).getField("settings").run();
 
-        // If welcome is off, don't proceed (don't welcome the user)
-        if (settings.welcomeEnabled !== "true") return;
+    // If welcome is off, don't proceed (don't welcome the user)
+    if (settings.welcomeEnabled !== "true") return;
 
-        // Replace the placeholders in the welcome message with actual data
-        const welcomeMessage = settings.welcomeMessage.replace("{{user}}", member.user.tag);
+    // Replace the placeholders in the welcome message with actual data
+    const welcomeMessage = settings.welcomeMessage.replace("{{user}}", member.user.tag);
 
-        // Send the welcome message to the welcome channel.
-        // There's a place for more configs here.
-        member.guild.channels.find("name", settings.welcomeChannel).send(welcomeMessage).catch(console.error);
-    }
+    // Send the welcome message to the welcome channel.
+    // There's a place for more configs here.
+    member.guild.channels.find("name", settings.welcomeChannel).send(welcomeMessage).catch(console.error);
+  }
 };
