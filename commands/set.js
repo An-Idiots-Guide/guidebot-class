a6da2e15d3706d79c1e416d164b83068446feee6
AndrewJWin <fastroute66@live.com>
2017-10-31 22:41:27 -0500

    Upload properly spaced versions

--- commands/set.js
+++ commands/set.js
@@ -15,79 +15,79 @@ const { inspect } = require("util");
 const Command = require("../base/Command.js");
 
 class Set extends Command {
-    constructor(client) {
-        super(client, {
-            name: "set",
-            description: "View or change settings for your server.",
-            category: "System",
-            usage: "set <view/get/edit> <key> <value>",
-            guildOnly: true,
-            aliases: ["setting", "settings", "conf"],
-            permLevel: "Administrator"
-        });
-    }
+  constructor(client) {
+    super(client, {
+      name: "set",
+      description: "View or change settings for your server.",
+      category: "System",
+      usage: "set <view/get/edit> <key> <value>",
+      guildOnly: true,
+      aliases: ["setting", "settings", "conf"],
+      permLevel: "Administrator"
+    });
+  }
 
-    async run(message, [action, key, ...value], level) { // eslint-disable-line no-unused-vars
+  async run(message, [action, key, ...value], level) { // eslint-disable-line no-unused-vars
 
-        // Retrieve current guild settings
-        const settings = await this.client.settings.get(message.guild.id).getField("settings").run();
+    // Retrieve current guild settings
+    const settings = await this.client.settings.get(message.guild.id).getField("settings").run();
   
-        // First, if a user does `-set add <key> <new value>`, let's add it
-        if (action === "add") {
-            if (!key) return message.reply("Please specify a key to add");
-            if (settings[key]) return message.reply("This key already exists in the settings");
-            if (value.length < 1) return message.reply("Please specify a value");
+    // First, if a user does `-set add <key> <new value>`, let's add it
+    if (action === "add") {
+      if (!key) return message.reply("Please specify a key to add");
+      if (settings[key]) return message.reply("This key already exists in the settings");
+      if (value.length < 1) return message.reply("Please specify a value");
 
-            // `value` being an array, we need to join it first.
-            settings[key] = value.join(" ");
+      // `value` being an array, we need to join it first.
+      settings[key] = value.join(" ");
   
-            // One the settings is modified, we write it back to the collection
-            await this.client.settings.get(message.guild.id).update({"settings":settings}).run();
-            message.reply(`${key} successfully added with the value of ${value.join(" ")}`);
-        } else
+      // One the settings is modified, we write it back to the collection
+      await this.client.settings.get(message.guild.id).update({"settings":settings}).run();
+      message.reply(`${key} successfully added with the value of ${value.join(" ")}`);
+    } else
   
-            // Secondly, if a user does `-set edit <key> <new value>`, let's change it
-            if (action === "edit") {
-                if (!key) return message.reply("Please specify a key to edit");
-                if (!settings[key]) return message.reply("This key does not exist in the settings");
-                if (value.length < 1) return message.reply("Please specify a new value");
+    // Secondly, if a user does `-set edit <key> <new value>`, let's change it
+    if (action === "edit") {
+      if (!key) return message.reply("Please specify a key to edit");
+      if (!settings[key]) return message.reply("This key does not exist in the settings");
+      if (value.length < 1) return message.reply("Please specify a new value");
     
-                settings[key] = value.join(" ");
+      settings[key] = value.join(" ");
 
-                await this.client.settings.get(message.guild.id).update({"settings":settings}).run();
-                message.reply(`${key} successfully edited to ${value.join(" ")}`);
-            } else
+      await this.client.settings.get(message.guild.id).update({"settings":settings}).run();
+      message.reply(`${key} successfully edited to ${value.join(" ")}`);
+    } else
   
-                // Thirdly, if a user does `-set del <key>`, let's ask the user if they're sure...
-                if (action === "del") {
-                    if (!key) return message.reply("Please specify a key to delete.");
-                    if (!settings[key]) return message.reply("This key does not exist in the settings");
+    // Thirdly, if a user does `-set del <key>`, let's ask the user if they're sure...
+    if (action === "del") {
+      if (!key) return message.reply("Please specify a key to delete.");
+      if (!settings[key]) return message.reply("This key does not exist in the settings");
       
-                    // Throw the 'are you sure?' text at them.
-                    const response = await this.client.awaitReply(message, `Are you sure you want to permanently delete ${key}? This **CANNOT** be undone.`);
+      // Throw the 'are you sure?' text at them.
+      const response = await this.client.awaitReply(message, `Are you sure you want to permanently delete ${key}? This **CANNOT** be undone.`);
 
-                    // If they respond with y or yes, continue.
-                    if (["y", "yes"].includes(response)) {
+      // If they respond with y or yes, continue.
+      if (["y", "yes"].includes(response)) {
 
-                        // We delete the `key` here.
-                        delete settings[key];
-                        await this.client.settings.get(message.guild.id).update({"settings":settings}).run();
-                        message.reply(`${key} was successfully deleted.`);
-                    } else
-                        // If they respond with n or no, we inform them that the action has been cancelled.
-                        if (["n","no","cancel"].includes(response)) {
-                            message.reply("Action cancelled.");
-                        }
-                } else
+        // We delete the `key` here.
+        delete settings[key];
+        await this.client.settings.get(message.guild.id).update({"settings":settings}).run();
+        message.reply(`${key} was successfully deleted.`);
+      } else
+      // If they respond with n or no, we inform them that the action has been cancelled.
+      if (["n","no","cancel"].includes(response)) {
+        message.reply("Action cancelled.");
+      }
+    } else
   
-                    if (action === "get") {
-                        if (!key) return message.reply("Please specify a key to view");
-                        if (!settings[key]) return message.reply("This key does not exist in the settings");
-                        message.reply(`The value of ${key} is currently ${settings[key]}`);
-                    } else {
-                        message.channel.send(inspect(settings), {code: "json"});
-                    }
+    if (action === "get") {
+      if (!key) return message.reply("Please specify a key to view");
+      if (!settings[key]) return message.reply("This key does not exist in the settings");
+      message.reply(`The value of ${key} is currently ${settings[key]}`);
+    } else {
+      message.channel.send(inspect(settings), {code: "json"});
     }
-    }
+  }
+}
 
-    module.exports = Set;
+module.exports = Set;
