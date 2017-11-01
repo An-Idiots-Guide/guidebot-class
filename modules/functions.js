0fb70970f29d6c3d42b7c474ecaf5a22626e5b97
AndrewJWin <fastroute66@live.com>
2017-10-31 22:42:13 -0500

    Replace file with properly spaced version

--- modules/functions.js
+++ modules/functions.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
 
-    /*
+  /*
     SINGLE-LINE AWAITMESSAGE
   
     A simple way to grab a single reply, from the user that initiated
@@ -12,19 +12,19 @@ module.exports = (client) => {
     msg.reply(`Oh, I really love ${response} too!`);
   
     */
-    client.awaitReply = async (msg, question, limit = 60000) => {
-        const filter = m=>m.author.id = msg.author.id;
-        await msg.channel.send(question);
-        try {
-            const collected = await msg.channel.awaitMessages(filter, { max: 1, time: limit, errors: ["time"] });
-            return collected.first().content;
-        } catch (e) {
-            return false;
-        }
-    };
+  client.awaitReply = async (msg, question, limit = 60000) => {
+    const filter = m=>m.author.id = msg.author.id;
+    await msg.channel.send(question);
+    try {
+      const collected = await msg.channel.awaitMessages(filter, { max: 1, time: limit, errors: ["time"] });
+      return collected.first().content;
+    } catch (e) {
+      return false;
+    }
+  };
 
 
-    /*
+  /*
     MESSAGE CLEAN FUNCTION
   
     "Clean" removes @everyone pings, as well as tokens, and makes code blocks
@@ -32,52 +32,52 @@ module.exports = (client) => {
     and stringifies objects!
     This is mostly only used by the Eval and Exec commands.
     */
-    client.clean = async (client, text) => {
-        if (text && text.constructor.name == "Promise")
-            text = await text;
-        if (typeof evaled !== "string")
-            text = require("util").inspect(text, {depth: 0});
+  client.clean = async (client, text) => {
+    if (text && text.constructor.name == "Promise")
+      text = await text;
+    if (typeof evaled !== "string")
+      text = require("util").inspect(text, {depth: 0});
 
-        text = text
-          .replace(/`/g, "`" + String.fromCharCode(8203))
-          .replace(/@/g, "@" + String.fromCharCode(8203))
-          .replace(client.token, "mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0");
+    text = text
+      .replace(/`/g, "`" + String.fromCharCode(8203))
+      .replace(/@/g, "@" + String.fromCharCode(8203))
+      .replace(client.token, "mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0");
 
-        return text;
-    };
+    return text;
+  };
 
 
-    /* MISCELANEOUS NON-CRITICAL FUNCTIONS */
+  /* MISCELANEOUS NON-CRITICAL FUNCTIONS */
   
-    // EXTENDING NATIVE TYPES IS BAD PRACTICE. Why? Because if JavaScript adds this
-    // later, this conflicts with native code. Also, if some other lib you use does
-    // this, a conflict also occurs. KNOWING THIS however, the following 2 methods
-    // are, we feel, very useful in code. 
+  // EXTENDING NATIVE TYPES IS BAD PRACTICE. Why? Because if JavaScript adds this
+  // later, this conflicts with native code. Also, if some other lib you use does
+  // this, a conflict also occurs. KNOWING THIS however, the following 2 methods
+  // are, we feel, very useful in code. 
   
-    // <String>.toPropercase() returns a proper-cased string such as: 
-    // "Mary had a little lamb".toProperCase() returns "Mary Had A Little Lamb"
-    String.prototype.toProperCase = function() {
-        return this.replace(/([^\W_]+[^\s-]*) */g, function(txt) {return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-    };
-    // <Array>.random() returns a single random element from an array
-    // [1, 2, 3, 4, 5].random() can return 1, 2, 3, 4 or 5.
-    Array.prototype.random = function() {
-        return this[Math.floor(Math.random() * this.length)];
-    };
+  // <String>.toPropercase() returns a proper-cased string such as: 
+  // "Mary had a little lamb".toProperCase() returns "Mary Had A Little Lamb"
+  String.prototype.toProperCase = function() {
+    return this.replace(/([^\W_]+[^\s-]*) */g, function(txt) {return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  };
+  // <Array>.random() returns a single random element from an array
+  // [1, 2, 3, 4, 5].random() can return 1, 2, 3, 4 or 5.
+  Array.prototype.random = function() {
+    return this[Math.floor(Math.random() * this.length)];
+  };
 
-    // `await client.wait(1000);` to "pause" for 1 second.
-    client.wait = require("util").promisify(setTimeout);
+  // `await client.wait(1000);` to "pause" for 1 second.
+  client.wait = require("util").promisify(setTimeout);
 
-    // These 2 process methods will catch exceptions and give *more details* about the error and stack trace.
-    process.on("uncaughtException", (err) => {
-        const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, "g"), "./");
-        console.error("Uncaught Exception: ", errorMsg);
-        // Always best practice to let the code crash on uncaught exceptions. 
-        // Because you should be catching them anyway.
-        process.exit(1);
-    });
+  // These 2 process methods will catch exceptions and give *more details* about the error and stack trace.
+  process.on("uncaughtException", (err) => {
+    const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, "g"), "./");
+    console.error("Uncaught Exception: ", errorMsg);
+    // Always best practice to let the code crash on uncaught exceptions. 
+    // Because you should be catching them anyway.
+    process.exit(1);
+  });
 
-    process.on("unhandledRejection", err => {
-        console.error("Uncaught Promise Error: ", err);
-    });
+  process.on("unhandledRejection", err => {
+    console.error("Uncaught Promise Error: ", err);
+  });
 };
