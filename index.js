dae2a657858b15e721fe53c5996d1887c84da80d
AndrewJWin <fastroute66@live.com>
2017-11-23 11:15:09 -0600

    Fix index.js

--- index.js
+++ index.js
@@ -191,4 +191,4 @@ init();
 client.on("disconnect", () => client.logger.warn("Bot is disconnecting..."))
   .on("reconnect", () => client.logger.log("Bot reconnecting...", "log"))
   .on("error", e => client.logger.error(e))
-.on("warn", info => client.logger.warn(info));
+  .on("warn", info => client.logger.warn(info));
