5406bf088a8979a28c252a375d92aad39e70079f
AndrewJWin <fastroute66@live.com>
2017-12-04 10:40:57 -0600

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -8,7 +8,7 @@ module.exports = class {
   async run(guild) {
     const def = await this.client.settings.get("default").getField("settings").run();
     this.client.user.setPresence({game: {name: `${def.prefix}help | ${this.client.guilds.size} Servers`, type:0}});
-    this.client.logger.log(`New guild has been joined: ${guild.name} (${guild.id}) with ${guild.memberCount - 1} members`, "JOINED");
+    this.client.logger.log(`New guild has been joined: ${guild.name} (${guild.id}) with ${guild.memberCount - 1} members`);
     this.client.settings.insert({"id":guild.id, "settings": def}).run();
   }
 };
