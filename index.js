fc9c84c970222670f40e262faec523891e064be3
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2019-07-19 19:08:08 +0300

    feat: add clean function in guidebot

--- index.js
+++ index.js
@@ -158,6 +158,29 @@ class GuideBot extends Client {
       return false;
     }
   }
+
+   /*
+  MESSAGE CLEAN FUNCTION
+  "Clean" removes @ everyone pings, as well as tokens, and makes code blocks
+  escaped so they're shown more easily. As a bonus it resolves promises
+  and stringifies objects!
+  This is mostly only used by the Eval and Exec commands.
+  */
+ async clean(text) {
+  if (text && text.constructor.name == "Promise")
+    text = await text;
+  if (typeof evaled !== "string")
+    text = require("util").inspect(text, {depth: 0});
+
+  text = text
+    .replace(/`/g, "`" + String.fromCharCode(8203))
+    .replace(/@/g, "@" + String.fromCharCode(8203))
+    .replace(this.token, "mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0");
+
+  return text;
+}
+
+
 }
 
 // This is your client. Some people call it `bot`, some people call it `self`,
